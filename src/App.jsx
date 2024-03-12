4eb5705b1e807f8a543eab1e6c5116238117d46d
Anne Katherine Rincon <akrincon@javerianacali.edu.co>
2024-03-12 16:53:15 -0500

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'    
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   <div className="login">
+    <form action="">
+      <h1>Login</h1>
+      <div className="input-box">
+        <input type="text" placeholder='Username' required />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="input-box">
+        <input type="Password" placeholder='Password' required />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="remember">
+        <label><input type="checkbox"/>Remember me</label>
+        <a href="#">Forgot Password?</a>
+      </div>
+      <button type='sunmit' className='but'> Login</button>
+    </form>
+
+   </div>
   )
 }
 
