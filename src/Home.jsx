720bda393a19f079953cf76fd76a3a022a4044e5
Anne Katherine Rincon <akrincon@javerianacali.edu.co>
2024-03-14 14:53:48 -0500

    ultimate

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Home.css'
 
 function HomePage() {
   return (
     <div>
-      <h1>¡Bienvenido a la página principal!</h1>
+      <h1>MARKET-KEEPER PRO</h1>
       {/* Aquí puedes agregar cualquier contenido que desees mostrar en la página principal */}
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './Login.jsx'
+import HomePage from './Home.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Login />
+      <HomePage />
     </BrowserRouter>
   </React.StrictMode>,
 )
