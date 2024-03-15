56c89d646536c5bd0753e65602539e96e7b2fa3d
Anne Katherine Rincon <akrincon@javerianacali.edu.co>
2024-03-15 13:53:49 -0500

    HomeUpdate

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,8 +12,21 @@ function HomePage() {
       <div style={{ display: 'flex' }}>
         <Sidebar />
         <div className='content'>
-          <h1>MARKET-KEEPER PRO</h1>
-          {/* Otro contenido */}
+          <h1 className="title">Market Keeper Pro</h1>
+          <p className="quote">"Optimiza tu negocio, simplifica tu éxito"</p>
+          <img src="/home.png" alt="ImageHome" className='imagen'/>
+          <div className="content">
+            <div className="right">
+              <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+              </ul>
+              <ul>
+                <li>Item 3</li>
+                <li>Item 4</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
